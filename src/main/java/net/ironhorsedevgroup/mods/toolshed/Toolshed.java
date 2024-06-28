73cf268d4a70447783ff84d0bdc6bea0aabb7462
Moonspire <21323568+Moonspire@users.noreply.github.com>
2024-06-27 20:27:39 -0600

    Added Start of Color tools

--- src/main/java/net/ironhorsedevgroup/mods/toolshed/Toolshed.java
+++ src/main/java/net/ironhorsedevgroup/mods/toolshed/Toolshed.java
@@ -2,34 +2,22 @@ package net.ironhorsedevgroup.mods.toolshed;
 
 import com.mojang.logging.LogUtils;
 import net.ironhorsedevgroup.mods.toolshed.interfaces.ItemRightClickTrigger;
-import net.minecraft.client.Minecraft;
+import net.ironhorsedevgroup.mods.toolshed.tools.Color;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
---
+++ src/main/java/net/ironhorsedevgroup/mods/toolshed/tools/Color.java
@@ -0,0 +1,7 @@
+package net.ironhorsedevgroup.mods.toolshed.tools;
+
+public class Color {
+    public static int getIntFromRGB(Integer red, Integer green, Integer blue) {
+        return ((red.byteValue() & 255) << 16) + ((green.byteValue() & 255) << 8) + (blue.byteValue() & 255);
+    }
+}
