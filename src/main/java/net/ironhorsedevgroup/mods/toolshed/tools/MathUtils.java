c18ac55dd7592a659dc363a8634b014ca41d879c
Moonspire <21323568+Moonspire@users.noreply.github.com>
2024-12-05 16:10:50 -0700

    Renamed Math class to not confuse with Java's in built Math class

--- src/main/java/net/ironhorsedevgroup/mods/toolshed/tools/MathUtils.java
+++ src/main/java/net/ironhorsedevgroup/mods/toolshed/tools/MathUtils.java
@@ -1,6 +1,6 @@
 package net.ironhorsedevgroup.mods.toolshed.tools;
 
-public class Math {
+public class MathUtils {
     public static Double getBellCurveAtX(double height, double width, double center, double x) {
         return height * java.lang.Math.pow(2, java.lang.Math.pow(x - center, 2) / (-width));
     }
