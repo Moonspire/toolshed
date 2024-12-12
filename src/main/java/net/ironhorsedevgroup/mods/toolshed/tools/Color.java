c020332d3a4bde0b42beb1a787d04366577e6b79
Moonspire <21323568+Moonspire@users.noreply.github.com>
2024-12-12 14:16:51 -0700

    Added new list based color method

--- src/main/java/net/ironhorsedevgroup/mods/toolshed/tools/Color.java
+++ src/main/java/net/ironhorsedevgroup/mods/toolshed/tools/Color.java
@@ -1,7 +1,13 @@
 package net.ironhorsedevgroup.mods.toolshed.tools;
 
+import java.util.List;
+
 public class Color {
     public static int getIntFromRGB(Integer red, Integer green, Integer blue) {
         return Integer.parseInt(String.valueOf(((red.byteValue() & 255) << 16) + ((green.byteValue() & 255) << 8) + (blue.byteValue() & 255)));
+    }
+
+    public static int getIntFromRGB(List<Integer> color) {
+        return getIntFromRGB(color.get(0), color.get(1), color.get(2));
     }
 }
